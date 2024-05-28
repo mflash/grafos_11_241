41efea28744d1316586c74d327d0d94645d01fe7
Marcelo Cohen <mflashbr@gmail.com>
2024-05-28 08:36:34 -0300

    Exercício dos filmes (final)

--- AppMovies.java
+++ AppMovies.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.util.Set;
 
 public class AppMovies {
@@ -24,6 +25,31 @@ public class AppMovies {
 		// System.out.println(g.toDot());
 		Set<String> nomes = g.getVerts();
 		// usar o conj. nomes para verificar se os nomes
-		// digitados realmente existem!	
+		// digitados realmente existem!
+		Scanner sc = new Scanner(System.in);
+		String nome1, nome2;
+		do {
+			System.out.print("Inicial: ");
+			nome1 = sc.nextLine();
+			if(!nomes.contains(nome1)) {
+				System.out.println("Nome não encontrado!");
+			}
+		} while(!nomes.contains(nome1));
+		do {
+			System.out.print("Final: ");
+			nome2 = sc.nextLine();
+			if(!nomes.contains(nome2)) {
+				System.out.println("Nome não encontrado!");
+			}
+		} while(!nomes.contains(nome2));
+		// Agora basta fazer um caminhamento por largura a partir do nome1!
+		System.out.println();
+		BFS bfs = new BFS(g, nome1);
+		if(bfs.hasPathTo(nome2)) {
+			for(String nome: bfs.pathTo(nome2)) {
+				System.out.println(nome);
+			}
+		}
+		System.out.println("O grafo é fortemente conexo? "+bfs.isStronglyConnected());
 	}
 }
--- BFS.java
+++ BFS.java
@@ -7,16 +7,20 @@ import java.util.Set;
 
 public class BFS {
 
+	private Graph g;
 	private Set<String> marked;
 	private Map<String, String> edgeTo;
 	private Map<String, Integer> distTo;
 	private String start;
+	private int totalVisitados;
 	
 	public BFS(Graph g, String start) {
 		this.start = start;
+		this.g = g;
 		marked = new HashSet<>();
 		edgeTo = new HashMap<>();
 		distTo = new HashMap<>();
+		totalVisitados = 0;
 		bfs(g, start);
 	}
 
@@ -42,6 +46,14 @@ public class BFS {
 		return distTo.get(v);
 	}
 
+	// Verifica se o grafo é fortemente conexo, ou seja,
+	// se é possível chegar a todos os vértices a partir de um qualquer
+	public boolean isStronglyConnected() {
+		System.out.println("Total visitados: "+totalVisitados);
+		System.out.println("Total vértices: "+g.totalVertices);
+		return totalVisitados == g.totalVertices;
+	}
+
 	public void bfs(Graph g, String s)
 	{
 		List<String> fila = new LinkedList<>();
@@ -50,7 +62,8 @@ public class BFS {
 		distTo.put(s, 0); // inicial está em distância 0
 		while(!fila.isEmpty()) {
 			s = fila.removeFirst();
-			System.out.println("Visitando: "+s);
+			totalVisitados++;
+			// System.out.println("Visitando: "+s);
 			for(String w: g.getAdj(s)) {
 				// Se não foi visitado ainda...
 				if(!marked.contains(w)) {
