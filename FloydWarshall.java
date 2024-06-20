5873561d6f8c7189b87a1a78b11d79a1e85f9ee8
Marcelo Cohen <mflashbr@gmail.com>
2024-06-20 09:17:10 -0300

    Implementação completa de Floyd-Warshall em Java

--- FloydWarshall.java
+++ FloydWarshall.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /* 
@@ -8,9 +10,9 @@ import java.util.Set;
 public class FloydWarshall {
   private static final String NEWLINE = System.getProperty("line.separator");
   private boolean temCicloNegativo; // tem ciclo negativo?
-  private double[][] dist; // dist[v][w] = distancia do caminho mais curto de v->w
-  private int[][] prev; // prev[v][w] = ultima aresta no caminho mais curto de v->w
-  private AdjMatrixEdgeWeightedDigraph g;
+  private Map<String, Double> dist; // dist[v][w] = distancia do caminho mais curto de v->w
+  private Map<String, String> prev; // prev[v][w] = ultima aresta no caminho mais curto de v->w
+  private EdgeWeightedDigraph g;
 
   private long startTime, endTime;
 
@@ -22,18 +24,54 @@ public class FloydWarshall {
    * 
    * @param G o digrafo valorado
    */
-  public FloydWarshall(AdjMatrixEdgeWeightedDigraph g) {
-    int V = g.getTotalVertices();
+  public FloydWarshall(EdgeWeightedDigraph g) {
+    int V = g.getTotalVerts();
     this.g = g;
-    dist = new double[V][V]; // inicialize todos com Double.POSITIVE_INFINITY
-    prev = new int[V][V]; // inicialize todos com -1
+    // dist = new double[V][V]; // inicialize todos com Double.POSITIVE_INFINITY
+    dist = new HashMap<>();
+    // prev = new int[V][V]; // inicialize todos com -1
+    prev = new HashMap<>();
+
+    Set<String> verts = g.getVerts();
+
+    // Inicialização "matrizes"
+    for (String v : verts) {
+      for (String w : verts) {
+        dist.put(v + "-" + w, Double.POSITIVE_INFINITY);
+        prev.put(v + "-" + w, null);
+        if (v.equals(w)) {
+          dist.put(v + "-" + w, 0.0);
+        }
+      }
+    }
+
+    // Monta "matrizes" iniciais
+    for (Edge e : g.getEdges()) {
+      String v = e.getV();
+      String w = e.getW();
+      double weight = e.getWeight();
+      dist.put(v + "-" + w, weight);
+      prev.put(v + "-" + w, v);
+    }
 
     // Comeco do algoritmo...
     startTime = System.currentTimeMillis();
 
     // Loop de Floyd-Warshall
-    for (int k = 0; k < V; k++) {
-        // ...
+    for (String k : verts) {
+      for (String i : verts) {
+        for (String j : verts) {
+          double dist_ikj = dist.get(i + "-" + k) + dist.get(k + "-" + j);
+          if (dist.get(i + "-" + j) > dist_ikj) {
+            dist.put(i + "-" + j, dist_ikj);
+            prev.put(i + "-" + j, prev.get(k + "-" + j));
+          }
+        }
+        if (dist.get(i + "-" + i) < 0) {
+          temCicloNegativo = true;
+          return;
+        }
+      }
     }
 
     // Fim do algoritmo
@@ -62,7 +100,7 @@ public class FloydWarshall {
    * @return {@code true} se existe um caminho
    */
   public boolean temCaminho(String s, String t) {
-    return true;
+    return dist.get(s + "-" + t) != Double.POSITIVE_INFINITY;
   }
 
   /**
@@ -76,7 +114,7 @@ public class FloydWarshall {
    * @throws UnsupportedOperationException se existir um ciclo negativo
    */
   public double dist(String s, String t) {
-    return -1;
+    return dist.get(s + "-" + t);
   }
 
   /**
@@ -93,18 +131,52 @@ public class FloydWarshall {
       throw new UnsupportedOperationException("Existe um ciclo negativo!");
     if (!temCaminho(u, v))
       return null;
+
     List<String> lista = new ArrayList<>();
+
     // Monte e retorne o caminho
+
+    lista.add(v);
+    while (!u.equals(v)) {
+      v = prev.get(u + "-" + v);
+      lista.add(0, v);
+    }
     return lista;
   }
 
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < dist.length; i++) {
-      for (int j = 0; j < dist[i].length; j++) {
-        if (prev[i][j] != -1)
-          sb.append(String.format("%5.2f ", dist[i][j]));
+    List<String> verts = new ArrayList<String>(g.getVerts());
+    sb.append("Distâncias:" + NEWLINE);
+    sb.append("  ");
+    for (String v : verts) {
+      sb.append(String.format("%-5s ", v));
+    }
+    sb.append(NEWLINE);
+    for (String v : verts) {
+      sb.append(v + " ");
+      for (String w : verts) {
+        if (prev.get(v + "-" + w) != null)
+          sb.append(String.format("%5.2f ", dist.get(v + "-" + w)));
+        else
+          sb.append("----- ");
+      }
+      sb.append(NEWLINE);
+    }
+
+    sb.append(NEWLINE);
+    sb.append("Caminhos:" + NEWLINE);
+    sb.append("  ");
+    for (String v : verts) {
+      sb.append(String.format("%-5s ", v));
+    }
+    sb.append(NEWLINE);
+    for (String v : verts) {
+      sb.append(v + " ");
+      for (String w : verts) {
+        if (prev.get(v + "-" + w) != null)
+          sb.append(String.format("%-5s ", prev.get(v + "-" + w)));
         else
           sb.append("----- ");
       }
@@ -122,16 +194,10 @@ public class FloydWarshall {
     // Carrega um grafo de um arquivo texto
     EdgeWeightedDigraph g = new EdgeWeightedDigraph("tinyEWD.txt");
 
-    // ...e cria um AdjMatrixEdgeWeightedDigraph...
-    AdjMatrixEdgeWeightedDigraph ag = new AdjMatrixEdgeWeightedDigraph(g);
-
-    System.out.println(ag);
-
     // Executa Floyd-Warshall
-    FloydWarshall fw = new FloydWarshall(ag);
+    FloydWarshall fw = new FloydWarshall(g);
 
     System.out.println();
-    System.out.println(fw);
 
     // Mostra todas as distâncias dos caminhos mais curtos
     // ...
@@ -144,18 +210,24 @@ public class FloydWarshall {
       // Exibe todos os caminhos
       for (String u : verts) {
         for (String v : verts) {
+          System.out.print(u + "->" + v + ": ");
           if (u != v && fw.temCaminho(u, v)) {
-            System.out.print(u + "->" + v + ": ");
             for (String x : fw.caminho(u, v)) {
               System.out.print(x + " ");
             }
-            System.out.println();
-          }
+            System.out.println(" (" + fw.dist(u, v) + ")");
+          } else
+            System.out.println(" *** Sem caminho");
         }
       }
     }
     System.out.println();
     System.out.println("Tempo de Floyd-Warshall: " + fw.tempoTotal());
+
+    System.out.println();
+    System.out.println(fw);
+
+    // System.out.println(g.toDot());
   }
 
 }
